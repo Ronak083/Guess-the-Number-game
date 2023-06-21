029e63036e0213763f7520c33e3cf74758ed5303
ronak <ronakgupta.official@gmail.com>
2023-06-21 09:59:12 +0530

    Project Setup For Guess the Number Game

---
+++ core/src/main/java/learn/spring/Main.java
@@ -0,0 +1,11 @@
+package learn.spring;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class Main {
+    private static final Logger log = LoggerFactory.getLogger(Main.class);
+    public static void main(String[] args) {
+        log.info("guess the Number Game");
+    }
+}
