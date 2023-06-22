88696e3a14b446c77e07b90e9a33398ef039e5bb
ronak <ronakgupta.official@gmail.com>
2023-06-22 12:37:05 +0530

    Congif Spring

--- core/src/main/java/learn/spring/Main.java
+++ core/src/main/java/learn/spring/Main.java
@@ -2,10 +2,21 @@ package learn.spring;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.context.support.AbstractRefreshableApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
 
 public class Main {
     private static final Logger log = LoggerFactory.getLogger(Main.class);
+    private static final String CONFIG_LOCATION = "beans.xml";
+
     public static void main(String[] args) {
         log.info("guess the Number Game");
+        ConfigurableApplicationContext context = new ClassPathXmlApplicationContext(CONFIG_LOCATION);
+        NumberGenerator numberGenerator = context.getBean("numberGenerator", NumberGenerator.class);
+        int number= numberGenerator.next();
+
+        log.info("number = {}",number);
+        context.close();
     }
 }
